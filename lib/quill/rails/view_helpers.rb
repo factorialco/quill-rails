db30f6957fd917ed71cd6e448c7e3f4a666860b7
andresgutgon <andresgutgon@gmail.com>
2017-02-20 18:26:20 +0100

    New Quill version

--- lib/quill/rails/view_helpers.rb
+++ lib/quill/rails/view_helpers.rb
@@ -6,10 +6,14 @@ module Quill
       def source_root
         File.dirname(__FILE__) + '/templates'
       end
+
       # A link helper to create a 'default' Quill text edit
-      #  
+      #
       def quill_editor(name=nil, options={})
-        mod_options = { name: 'quill-value', id: 'quill-value' }.merge options
+        mod_options = {
+          name: 'quill-value',
+          id: 'quill-value'
+        }.merge options
         @input_name = (name || mod_options[:name])
         @input_id   = quill_sanitize_id(name || mod_options[:id])
         @value      = mod_options[:value].present? ? mod_options[:value] : ""
